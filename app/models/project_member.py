f8694fbc66b6c11a28252563948171e34058570d
sajidzeya125 <sajidzeya294@gmail.com>
2026-05-01 18:07:20 +0530

    recent update

--- app/models/project_member.py
+++ app/models/project_member.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, ForeignKey, UniqueConstraint
 from app.database import Base
 class ProjectMember(Base):
     __tablename__ = "project_members"
-    __table_args__ = (UniqueConstraint("project_id", "user_id", name="uix_project_user"),)
+    __table_args__ = (UniqueConstraint("project_id", "user_id", name="uix_project_user"))
 
     id = Column(Integer, primary_key=True, index=True)
     project_id = Column(Integer, ForeignKey("projects.id"), nullable=False)
